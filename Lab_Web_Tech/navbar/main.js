bff681257d6a0574bf063d430633d64ee3bc7e14
Naresh Jaipal <132568227+NareshJaipal@users.noreply.github.com>
2024-02-23 20:36:58 +0500

    created Lab and created pages and routes.

---
+++ Lab_Web_Tech/navbar/main.js
@@ -0,0 +1,28 @@
+function loadNavbar() {
+  fetch("./navbar/navbar.html")
+    .then((response) => response.text())
+    .then((html) => {
+      const navbarContainer = document.getElementById("navbar");
+      if (navbarContainer) {
+        navbarContainer.innerHTML = html;
+        addMenuFunctionality();
+      }
+    })
+    .catch((error) => console.error("Error fetching navbar:", error));
+}
+
+function addMenuFunctionality() {
+  const menuBtn = document.querySelector(".menu-icon-btn");
+  menuBtn.addEventListener("click", function () {
+    const menuItems = document.getElementById("menu-items");
+    if (menuItems.classList.contains("active")) {
+      menuItems.classList.add("disActive");
+      menuItems.classList.remove("active");
+    } else {
+      menuItems.classList.remove("disActive");
+      menuItems.classList.add("active");
+    }
+  });
+}
+
+window.onload = loadNavbar;
